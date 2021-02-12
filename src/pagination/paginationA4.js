86d0697d53a4426cb0459b29751bf2a731c3a847
Tunmishe Israel <aitboy18@gmail.com>
2021-02-12 20:53:22 +0100

    make it better

--- src/pagination/paginationA4.js
+++ src/pagination/paginationA4.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Header from '../header/header'
+import {ReactComponent as Up } from '../images/up-arrow.svg'
+import {ReactComponent as Down } from '../images/down-arrow.svg'
 import { NavLink } from 'react-router-dom'
 import Button from '../components/button'
 import Footer from '../components/footer'
@@ -9,7 +11,28 @@ import PaginationPathA from './paginationPathA'
 
 const PaginationA4 = () => {
 
-    
+    const [ click, setClick ] = useState({
+        days: false,
+        date: false
+    })
+
+    const handleDaysClick = () => {
+        // console.log(click.days)
+        const daysClick = {
+            ...click,
+            days: !click.days
+        }
+        setClick(daysClick)
+    }
+
+    const handleDateClick = () => {
+        // console.log(click.date)
+        const dateClick = {
+            ...click,
+            date: !click.date,
+        } 
+        setClick(dateClick)
+    }
 
     return (
         <div className="primary">
@@ -18,8 +41,48 @@ const PaginationA4 = () => {
                 <div className="pagination">
                     <PaginationPathA/>
                 </div>
-                <div>
-
+                <div className="calc">
+                    <div>
+                        <div className="calc-inner">
+                            <p>Remaining days</p>
+                            <div className="calc-data">37 days to go</div>
+                            <div className="unit">
+                                <div>Unit</div>
+                                <div className="arrow" onClick={handleDaysClick}>
+                                    {click.days ? (
+                                        <Up/>
+                                    ) : (
+                                        <Down/>
+                                    )}
+                                </div>
+                                {/* <div className="arrow"><Down/></div> */}
+                            </div>
+                        </div>
+                        <div>
+                            <Button text="Give Feedback"/>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="calc-inner">
+                            <p>Remaining days</p>
+                            <div className="calc-data">25th October 2021</div>
+                            <div className="unit">
+                                <div>End data</div>
+                                <div className="arrow" onClick={handleDateClick}>
+                                    {click.date ? (
+                                        <Up/>
+                                    ) : (
+                                        <Down/>
+                                    )}
+                                </div>
+                                {/* <div className="arrow"><Down/></div> */}
+                            </div>
+                        </div>
+                        <div className="dormant">
+                            <Button text="Top-up unit" />
+                            <p>*coming soon*</p>
+                        </div>
+                    </div>
                 </div>
                 <Footer/>
             </div>
--- src/pagination/paginationB6.js
+++ src/pagination/paginationB6.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Header from '../header/header'
+import {ReactComponent as Up } from '../images/up-arrow.svg'
+import {ReactComponent as Down } from '../images/down-arrow.svg'
+import { NavLink } from 'react-router-dom'
+import Button from '../components/button'
 import Footer from '../components/footer'
 import PaginationPathB from './paginationPathB'
-import Input from '../components/input'
 
-const Pagination6 = () => {
+
+
+const PaginationB6 = () => {
+
+    const [ click, setClick ] = useState({
+        days: false,
+        date: false
+    })
+
+    const handleDaysClick = () => {
+        // console.log(click.days)
+        const daysClick = {
+            ...click,
+            days: !click.days
+        }
+        setClick(daysClick)
+    }
+
+    const handleDateClick = () => {
+        // console.log(click.date)
+        const dateClick = {
+            ...click,
+            date: !click.date,
+        } 
+        setClick(dateClick)
+    }
 
     return (
         <div className="primary">
@@ -13,12 +41,53 @@ const Pagination6 = () => {
                 <div className="pagination">
                     <PaginationPathB/>
                 </div>
-
+                <div className="calc">
+                    <div>
+                        <div className="calc-inner">
+                            <p>Remaining days</p>
+                            <div className="calc-data">37 days to go</div>
+                            <div className="unit">
+                                <div>Unit</div>
+                                <div className="arrow" onClick={handleDaysClick}>
+                                    {click.days ? (
+                                        <Up/>
+                                    ) : (
+                                        <Down/>
+                                    )}
+                                </div>
+                                {/* <div className="arrow"><Down/></div> */}
+                            </div>
+                        </div>
+                        <div>
+                            <Button text="Give Feedback"/>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="calc-inner">
+                            <p>Remaining days</p>
+                            <div className="calc-data">25th October 2021</div>
+                            <div className="unit">
+                                <div>End data</div>
+                                <div className="arrow" onClick={handleDateClick}>
+                                    {click.date ? (
+                                        <Up/>
+                                    ) : (
+                                        <Down/>
+                                    )}
+                                </div>
+                                {/* <div className="arrow"><Down/></div> */}
+                            </div>
+                        </div>
+                        <div className="dormant">
+                            <Button text="Top-up unit" />
+                            <p>*coming soon*</p>
+                        </div>
+                    </div>
+                </div>
                 <Footer/>
             </div>
         </div>
-            
     )
 }
 
-export default Pagination6
+export default PaginationB6
